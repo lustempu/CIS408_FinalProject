9b9370a394f6221c5986ec956168ce459836665e
lustempu <l.stempuzis@vikes.csuohio.edu>
2023-05-01 21:32:50 -0400

    add to cart button in popup, AJAX/jQuery for backend stuff

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
+// Declare a global variable to store the product data
+let productData = [];
+
 async function displayData() {
   try {
     const response = await fetch("php/get_data.php");
-
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const shirts = await response.json();
+    productData = shirts; // Save the fetched product data to the global variable
     shirts.forEach((shirt, index) => {
       const name = shirt.NAME;
       const description = shirt.DESCRIPTION;
-
       let productNameElement = document.getElementById(
         `product-name-${index + 1}`
       );
       let productDescriptionElement = document.getElementById(
         `product-description-${index + 1}`
       );
-
       if (productNameElement && productDescriptionElement) {
         productNameElement.textContent = name;
         productDescriptionElement.textContent = description;
@@ -25,4 +26,16 @@ async function displayData() {
   } catch (error) {
     console.error("Error fetching shirt data:", error);
   }
+}
+
+function cartAction(para) {
+  if (para === "add") {
+    alert("Item added to cart!");
+  } else if (para === "remove") {
+    alert("Item removed from cart!");
+  }
+}
+
+function emptyCart() {
+  alert("Cart emptied!");
 }
--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,24 @@
-function openPopup(parentElement) {
+function openPopup(parentElement, index) {
   var popup = document.getElementById("popup");
   var popupText = document.querySelector(".popupText");
   var popupTitle = document.querySelector(".popupTitle");
   var popupPrice = document.querySelector(".popupPrice");
+  var popupSizes = document.querySelector(".popupSizes");
   var img = parentElement.querySelector(".card-img-top");
   var title = parentElement.querySelectorAll(".card-title");
   var text = parentElement.querySelectorAll(".card-text");
 
-  var price = "Price: $5.99";
+  // Fetch the price and size from the global productData variable
+  var price = productData[index].PRICE;
+  var size = productData[index].SIZES;
+
   popupTitle.innerHTML = title[0].innerHTML;
   popupText.innerHTML = text[0].innerHTML;
-  popupPrice.innerHTML = price;
+  popupPrice.innerHTML = `Price: $${price}`;
+  popupSizes.innerHTML = `Size: ${size}`;
   popup.classList.add("open-popup");
 }
+
 function closePopup() {
   var popup = document.getElementById("popup");
   popup.classList.remove("open-popup");
