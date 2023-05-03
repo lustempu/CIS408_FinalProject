5065bb265947ffd6adbd8ebc471d6d7e41c87a7b
lustempu <l.stempuzis@vikes.csuohio.edu>
2023-05-03 15:41:06 -0400

    add name and desc ids for all cards, openPopup() now has 3 params

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
-// Declare a global variable to store the product data
-let productData = [];
+let productData = []; // Declare a global variable to store the product data
+// now we can use the productData variable in the displayData function
 
 async function displayData() {
+  //this displays the backend data for the cards, but not the popups
+  // Fetch the product data from the database
   try {
     const response = await fetch("php/get_data.php");
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const productDescriptionElement = await response.json();
-    productData = productDescriptionElement; // Save the fetched product data to the global variable
+    productData = productDescriptionElement; // the data is returned as an array of objects, so store it in the global variable
     productDescriptionElement.forEach((item, index) => {
-      const name = item.NAME;
-      const description = item.DESCRIPTION;
+      // Loop through the array of objects
+      const name = item.NAME; // Get the name from the current object
+      const description = item.DESCRIPTION; // Get the description from the current object
       let productNameElement = document.getElementById(
-        `product-name-${index + 1}`
+        // Get the product name element
+        `product-name-${index + 1}` // the index starts at 0, but the product names start at 1, so add 1 to the index
       );
       let productDescriptionElement = document.getElementById(
         `product-description-${index + 1}`
--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,21 @@
-function openPopup(parentElement, index) {
+async function openPopup(parentElement, index, imgElement) {
+  // console.log("imgElement " + imgElement); // debugging purposes --> issues resolved!
   var popup = document.getElementById("popup");
   var popupText = document.querySelector(".popupText");
   var popupTitle = document.querySelector(".popupTitle");
   var popupPrice = document.querySelector(".popupPrice");
   var popupSizes = document.querySelector(".popupSizes");
-  var img = parentElement.querySelector(".card-img-top");
+  var popupImage = document.querySelector(".popupImage");
   var title = parentElement.querySelectorAll(".card-title");
   var text = parentElement.querySelectorAll(".card-text");
-
   // Fetch the price and size from the global productData variable
   var price = productData[index].PRICE;
   var size = productData[index].SIZES;
-
   popupTitle.innerHTML = title[0].innerHTML;
   popupText.innerHTML = text[0].innerHTML;
   popupPrice.innerHTML = `Price: $${price}`;
   popupSizes.innerHTML = `Size: ${size}`;
+  popupImage.src = imgElement.src; // Update the img src
   popup.classList.add("open-popup");
 }
 
